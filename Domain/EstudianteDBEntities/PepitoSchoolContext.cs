8287b5dacfb2b105b349663576fb311ddd6c357d
AbdielGE <86158145+AbdielGarc1a@users.noreply.github.com>
2022-05-19 23:52:13 -0600

    Add files via upload

---
+++ Domain/EstudianteDBEntities/PepitoSchoolContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Domain.Entities;
+
+#nullable disable
+
+namespace Domain.EstudianteDBEntities
+{
+    public partial class PepitoSchoolContext : DbContext
+    {
+        public PepitoSchoolContext()
+        {
+        }
+
+        public PepitoSchoolContext(DbContextOptions<PepitoSchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Estudiante> Estudiantes { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Data Source=DESKTOP-79HMS7E;Initial Catalog=PepitoSchool;user=sa;password=123456");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudiante>(entity =>
+            {
+                entity.ToTable("Estudiante");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Apellidos)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false)
+                    .HasColumnName("apellidos");
+
+                entity.Property(e => e.Carnet)
+                    .IsRequired()
+                    .HasMaxLength(12)
+                    .IsUnicode(false)
+                    .HasColumnName("carnet");
+
+                entity.Property(e => e.Contabilidad).HasColumnName("contabilidad");
+
+                entity.Property(e => e.Correo)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("correo");
+
+                entity.Property(e => e.Direccion)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("direccion");
+
+                entity.Property(e => e.Estadistica).HasColumnName("estadistica");
+
+                entity.Property(e => e.Matematica).HasColumnName("matematica");
+
+                entity.Property(e => e.Nombres)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false)
+                    .HasColumnName("nombres");
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(16)
+                    .IsUnicode(false)
+                    .HasColumnName("phone");
+
+                entity.Property(e => e.Programacion).HasColumnName("programacion");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
